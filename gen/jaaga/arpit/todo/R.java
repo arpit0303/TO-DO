739ce3427397a053028e0293ba7b59598f43b071
Arpit <arpitagrawal910@gmail.com>
2014-08-15 01:10:11 +0530

    ListView with card look

--- gen/jaaga/arpit/todo/R.java
+++ gen/jaaga/arpit/todo/R.java
@@ -29,7 +29,10 @@ public final class R {
         public static final int done=0x7f020000;
         public static final int ic_action=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int reminder=0x7f020003;
+        public static final int layer_card_background=0x7f020003;
+        public static final int layer_card_background_selected=0x7f020004;
+        public static final int reminder=0x7f020005;
+        public static final int selector_card_background=0x7f020006;
     }
     public static final class id {
         public static final int action_delete=0x7f090007;
